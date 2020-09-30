213545d3f5dd27956eeb85f22fb4dc11166de399
Shivam Patel <shivampatel251097@gmail.com>
2020-09-30 16:34:24 +0530

    event and property binding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [  new Recipe('A Test recipe','This is simply a Test','https://www.indianhealthyrecipes.com/wp-content/uploads/2019/11/samosa-recipe.jpg'),
-  new Recipe('A another Test recipe','This is simply a another Test','https://www.indianhealthyrecipes.com/wp-content/uploads/2019/11/samosa-recipe.jpg')
+  recipes: Recipe[] = [  new Recipe('Samosa recipe', 'A crunchy combination of potato masala and Papdi, fried in oil.', 'https://www.indianhealthyrecipes.com/wp-content/uploads/2019/11/samosa-recipe.jpg'),
+  new Recipe('Dosa recipe', 'A south Indian dish. we eat with Sambhar and Coconut chatni', 'https://spiderimg.amarujala.com/assets/images/2019/07/01/750x506/dosa_1561969359.jpeg')
 ];
 
   constructor() { }
